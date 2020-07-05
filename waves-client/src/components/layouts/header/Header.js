a53943f6bff702796a537e6a8eb3bc876ae46de5
joker7t <batoan131196@gmail.com>
2020-07-05 21:52:54 +0700

    add header footer

--- waves-client/src/components/layouts/header/Header.js
+++ waves-client/src/components/layouts/header/Header.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className='bck_b_light'>
+            <div className='container'>
+                <div className='left'>
+                    <div className='logo'>
+                        Waves
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className='top'>
+                        top
+                    </div>
+                    <div className='bottom'>
+                        bottom
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
