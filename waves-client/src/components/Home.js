57bbe433a0b60f863ced463e5b0bb9d3a8f1a125
joker7t <batoan131196@gmail.com>
2020-07-08 21:05:39 +0700

    add home promotion

--- waves-client/src/components/Home.js
+++ waves-client/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeSlider from './HomeSlider';
+import HomePromotion from './HomePromotion';
 
 const Home = () => {
     return (
         <div className='page_container'>
             <HomeSlider />
+            <HomePromotion />
         </div>
     );
 }
--- waves-client/src/components/HomePromotion.js
+++ waves-client/src/components/HomePromotion.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import Feature3 from '../images/featured/featured_home_3.jpg';
+import Slider from 'react-slick';
+import Button from '../utils/Button';
 
 const HomePromotion = () => {
-    return (
-        <div>
 
+    const promotion = {
+        img: Feature3,
+        lineOne: 'Up to 40% off',
+        lineTwo: 'In second hand guitars',
+        linkTitle: 'Shop now',
+        linkTo: '/shop'
+    };
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+    };
+
+    const renderPromotions = () => (
+        <div>
+            <div
+                className='home_promotion_img'
+                style={{
+                    background: `url(${promotion.img})`,
+                    width: `100%`
+                }}
+            >
+                <div className='featured_action'>
+                    <div className='tag title'>{promotion.lineOne}</div>
+                    <div className='tag low_title'>{promotion.lineTwo}</div>
+                    <div>
+                        <Button
+                            type='default'
+                            title={promotion.linkTitle}
+                            linkTo={promotion.linkTo}
+                            addStyles={{
+                                margin: '10px 0 0 0'
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+    return (
+        <div className='home_promotion'>
+            <Slider {...settings}>
+                {renderPromotions()}
+            </Slider>
         </div>
     );
 }
