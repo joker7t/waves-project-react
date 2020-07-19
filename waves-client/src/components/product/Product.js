944c5b348a2a8ab5418ae9760f27939422a86bb2
joker7t <batoan131196@gmail.com>
2020-07-19 12:34:19 +0700

    done basic info in product detail

--- waves-client/src/components/product/Product.js
+++ waves-client/src/components/product/Product.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { setSelectedProduct } from '../../actions/productAction';
+import PageTop from '../../utils/PageTop';
+import Loader from '../../utils/Loader';
+import ProductInfo from './ProductInfo';
 
-const Product = ({ history, match, setSelectedProduct }) => {
+const Product = ({ history, match, setSelectedProduct, selectedProduct }) => {
 
     useEffect(() => {
         const loadData = async () => {
@@ -18,14 +21,43 @@ const Product = ({ history, match, setSelectedProduct }) => {
 
         loadData();
 
+        return () => {
+            setSelectedProduct(null);
+        };
         //eslint-disable-next-line
     }, []);
 
+    const addToCartHandler = () => {
+        console.log('add to cart');
+    }
+
     return (
         <div className='page_container'>
-
+            <PageTop title='Product detail' />
+            <div className='container'>
+                {
+                    selectedProduct ?
+                        <div className='product_detail_wrapper'>
+                            <div className='left'>
+                                images
+                            </div>
+                            <div className='right'>
+                                <ProductInfo
+                                    addToCart={addToCartHandler}
+                                    product={selectedProduct}
+                                />
+                            </div>
+                        </div>
+                        :
+                        <Loader />
+                }
+            </div>
         </div>
     );
 }
 
-export default connect(null, { setSelectedProduct })(Product);
+const mapStateToProps = (state) => ({
+    selectedProduct: state.product.selectedProduct
+});
+
+export default connect(mapStateToProps, { setSelectedProduct })(Product);
---
+++ waves-client/src/components/product/ProductInfo.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import faTruck from '@fortawesome/fontawesome-free-solid/faTruck';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
+import faShoppingBag from '@fortawesome/fontawesome-free-solid/faShoppingBag';
+
+const ProductInfo = ({ product, addToCart }) => {
+
+    const { name, brand, description, shipping, available, price, frets, wood } = product;
+    return (
+        <div>
+            <h1>{brand.name} {name}</h1>
+            <p>{description}</p>
+            <div className='product_tags'>
+                {shipping ?
+                    <div className='tag'>
+                        <div><FontAwesomeIcon icon={faTruck} /></div>
+                        <div className='tag_text'>
+                            <div>Free shipping</div>
+                            <div>And return</div>
+                        </div>
+                    </div>
+                    :
+                    null
+                }
+                {available ?
+                    <div className='tag'>
+                        <div><FontAwesomeIcon icon={faCheck} /></div>
+                        <div className='tag_text'>
+                            <div>Available</div>
+                            <div>In store</div>
+                        </div>
+                    </div>
+                    :
+                    <div className='tag'>
+                        <div><FontAwesomeIcon icon={faTimes} /></div>
+                        <div className='tag_text'>
+                            <div>Not available</div>
+                            <div>Pre-oder only</div>
+                        </div>
+                    </div>
+                }
+            </div>
+            <div className='product_actions'>
+                <div className='price'>$ {price}</div>
+                <div className='cart'>
+                    <div
+                        className='add_to_cart_link'
+                        action={addToCart}
+                    >
+                        <FontAwesomeIcon icon={faShoppingBag} />Add to cart
+                    </div>
+                </div>
+            </div>
+            <div className='product_specifications'>
+                <h2>Specifications: </h2>
+                <div>
+                    <div className='item'>
+                        <strong>Frets: </strong>{frets}
+                    </div>
+                    <div className='item'>
+                        <strong>Wood: </strong>{wood.name}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductInfo;
