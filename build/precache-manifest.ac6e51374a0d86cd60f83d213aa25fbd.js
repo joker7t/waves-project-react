b66f092adcd5069109730c187331157b1bbc3a35
joker7t <batoan131196@gmail.com>
2020-07-26 15:17:37 +0700

    deploy to heroku

---
+++ build/precache-manifest.ac6e51374a0d86cd60f83d213aa25fbd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ccb30071428caf9271e201f03cfcc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71cfb0d9a0bb481a5e27",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "e07801e6b573d6b949cd",
+    "url": "/static/css/main.0a2f11fa.chunk.css"
+  },
+  {
+    "revision": "71cfb0d9a0bb481a5e27",
+    "url": "/static/js/2.a2f02816.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/static/js/2.a2f02816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07801e6b573d6b949cd",
+    "url": "/static/js/main.b2cd207e.chunk.js"
+  },
+  {
+    "revision": "214b5bcb0beed5af2c8d",
+    "url": "/static/js/runtime-main.3ef0f2c6.js"
+  },
+  {
+    "revision": "f175cd096f1f4fc37c9fe68014a79fff",
+    "url": "/static/media/featured_home.f175cd09.jpg"
+  },
+  {
+    "revision": "e11708c4720934ce9195f885d35dddf8",
+    "url": "/static/media/featured_home_2.e11708c4.jpg"
+  },
+  {
+    "revision": "b0762faa541d252cef7d2bb1f915b6b7",
+    "url": "/static/media/featured_home_3.b0762faa.jpg"
+  },
+  {
+    "revision": "f0e230d72b9a88a856bb3620b6d2fd8b",
+    "url": "/static/media/image_not_availble.f0e230d7.png"
+  }
+]);
--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/sites', require('./routes/sites'));
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
