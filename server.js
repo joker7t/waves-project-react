1a6f1ae9599ea91e0687c897f21a42382ae5a331
joker7t <batoan131196@gmail.com>
2020-07-26 15:35:51 +0700

    fix compile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
